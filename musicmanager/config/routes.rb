3ca9ff39181f2798d026297af24ffe24d7321c06
saicharan2410 <saicharan2410@gmail.com>
2014-12-20 18:22:43 -0600

    Created Misuc
    Page

--- musicmanager/config/routes.rb
+++ musicmanager/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :musics
+
   resources :statuses
 
   # The priority is based upon order of creation: first created -> highest priority.
--- musicmanager/db/schema.rb
+++ musicmanager/db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141220235821) do
+ActiveRecord::Schema.define(version: 20141221002106) do
+
+  create_table "musics", force: :cascade do |t|
+    t.string   "name"
+    t.string   "format"
+    t.string   "length"
+    t.string   "size"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "statuses", force: :cascade do |t|
     t.string   "name"
