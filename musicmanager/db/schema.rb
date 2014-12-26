a3784545a35fbd2ba304c643d9152adeaa7f69e5
saicharan2410 <saicharan2410@gmail.com>
2014-12-25 18:28:06 -0600

    Postgre migrate
    Migrated to postgresql and created a new gemlock file

--- musicmanager/db/schema.rb
+++ musicmanager/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141221002106) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "musics", force: :cascade do |t|
     t.string   "name"
     t.string   "format"
