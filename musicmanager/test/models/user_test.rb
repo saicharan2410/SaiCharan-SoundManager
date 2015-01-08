152ce90d75fea9e4f093ac2702c9150b5d7f5ea2
saicharan2410 <saicharan2410@gmail.com>
2015-01-08 16:58:14 -0600

    Routes
    Created custom routes

--- musicmanager/test/models/user_test.rb
+++ musicmanager/test/models/user_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+	test "a user should enter a mail id" do
+		user = User.new
+		assert !user.save
+	end
   # test "the truth" do
   #   assert true
   # end
