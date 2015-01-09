6d92ac467d6e8f4b2694351407169e9af982f083
saicharan2410 <saicharan2410@gmail.com>
2015-01-08 22:42:14 -0600

    removed add files
    User test cases added

--- musicmanager/test/models/user_test.rb
+++ musicmanager/test/models/user_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-	test "a user should enter a mail id" do
-		user = User.new
-		assert !user.save
-	end
   # test "the truth" do
   #   assert true
   # end
